c2c9065c655ba389e974c8cc6cc98bd03b975335
Matheus Almeida <matheuscsalmeida@gmail.com>
2022-01-06 17:03:54 -0300

    Adicionando input para setar o tamanho da box

--- src/templates/LandingPages/LpEducacao/index.jsx
+++ src/templates/LandingPages/LpEducacao/index.jsx
@@ -3,31 +3,25 @@ import Header from '../../../components/RetornoLP/HeaderLP';
 import './style.css'
 
 function LpEducacao() {
-  const [valorWidth, setValorWidth] = useState(10);
-  const [styles, setStyles] = useState();
+  const [valorWidth, setValorWidth] = useState(0);
 
   const titulo = 'LP de Educação'
 
   const handleValorWidth = (event) => {
-    setValorWidth({valorWidth: event.target.value})
+    setValorWidth(event.target.value)
     console.log(valorWidth);
   };
   
-useEffect(()=>{
-  setStyles({
-    styles: {
-      width: valorWidth + 'px',
-      height: 200 + 'px',
-    }
-  }) 
-  
-}, [valorWidth]);
+const styles = {
+    width: 350 + 'px',
+    height: valorWidth + 'px',
+  }
 
   return (
     < section >
       <Header titulo={titulo} />
       <div className='div-teste' style={styles} >
-      teste
+        teste
       </div>
       <input type="number" placeholder='Numero' onChange={handleValorWidth} />
     </section>
