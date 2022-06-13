27ef274d6135a4be1a39ea95d624ba3ea8867831
Matheus Almeida <matheuscsalmeida@gmail.com>
2022-06-12 22:41:10 -0300

    Commit para verificar hospedagem

--- src/templates/LandingPages/LpEducacao/index.jsx
+++ src/templates/LandingPages/LpEducacao/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../../../components/RetornoLP/HeaderLP';
 import './style.css'
 import TemplateLpEducacao from './TemplateLpEducacao';
